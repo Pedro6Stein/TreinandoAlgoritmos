8ec2d8c4b3c3b16c11a5c57779db5f9dd0b36a2b
Pedro Stein <65890570+Pedro6Stein@users.noreply.github.com>
2025-04-11 16:22:17 -0300

    Alguns_Exercicio_De_Olimpiada

--- Revisao/src/Main.java
+++ Revisao/src/Main.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 //https://www.inf.pucrs.br/~pinho/LaproI/Exercicios/SeqDecisao/lista1.htm
 
+
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
     }
+
     public static void dois_Pontos() {
         //Construa um algoritmo que, tendo como dados de entrada
         // dois pontos quaisquer no plano, P(x1,y1) e P(x2,y2),
@@ -24,7 +26,8 @@ public class Main {
         d = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         System.out.println("A distancia entre os pontos é: " + d);
     } //Exercicio 1
-    public  static void vendedor (){
+
+    public static void vendedor() {
         //Um vendedor necessita de um algoritmo
         // que calcule o preço total devido por um cliente.
         // O algoritmo deve receber o código de um produto
@@ -37,9 +40,10 @@ public class Main {
         //0987	5,32
         //7623	6,45
 
-        int [] cod = {1001,1324,6548,987,7623};
-        float [] valor = {5.32f,6.45f,2.37f,5.32f,6.45f};
-        int qtd, codigoEntrada ; float total=0;
+        int[] cod = {1001, 1324, 6548, 987, 7623};
+        float[] valor = {5.32f, 6.45f, 2.37f, 5.32f, 6.45f};
+        int qtd, codigoEntrada;
+        float total = 0;
 
         System.out.println("Qual item comprou?(digite o cod):");
         codigoEntrada = scanner.nextInt();
@@ -47,20 +51,74 @@ public class Main {
         qtd = scanner.nextInt();
 
         boolean encotrado = false;
-        for (int i = 0; i < cod.length; i++ ){
+        for (int i = 0; i < cod.length; i++) {
             if (cod[i] == codigoEntrada) {
                 total = qtd * valor[i];
                 encotrado = true;
                 break;
             }
         }
-        if(encotrado) {
-            System.out.println("O preço total do produto é "+ total);
-        }else {
+        if (encotrado) {
+            System.out.println("O preço total do produto é " + total);
+        } else {
             System.out.println("Não encotrado");
         }
 
+    } //Exercicio 2
+
+    public static void piloto() {
+        //Link do exercicio:
+        //https://olimpiada.ic.unicamp.br/pratique/pj/2020/f1/piloto/
+
+//        Piloto Automático
+//        Uma grande fábrica de carros elétricos está realizando melhorias no sistema
+//        de piloto automático e precisa da sua ajuda para implementar um programa que decida
+//        se um carro B, que está trafegando no meio de dois carros A e C, precisa acelerar, desacelerar ou manter
+//        a velocidade atual. Os carros são iguais e os sensores do piloto automático vão fornecer, como entrada, a
+//        posição atual da traseira dos três carros. Veja um exemplo na figura.
+//
+//                O carro B precisa ser acelerado se a distância da sua traseira para a traseira do carro A for menor
+//                do que a distância da sua traseira para a traseira do carro C. Se for maior, ele precisa ser desacelerado.
+//                Se for igual, precisa manter a velocidade atual. Quer dizer,
+//                o carro B precisa ser acelerado se (B-A) < (C-B), desacelerado se (B-A) > (C-B) e manter a velocidade se (B-A) for igual a (C-B).
+//
+//                Entrada
+//        A primeira linha da entrada contém um inteiro A. A segunda linha da entrada contém um inteiro B.
+//        A terceira linha da entrada contém um inteiro C. Os três inteiros representam as posições atuais das traseiras dos carros A, B e C, respectivamente.
+//
+//        Saída
+//        Seu programa deve imprimir uma linha contendo um inteiro: 1 se o carro B precisa acelerar; -1 se precisa desacelerar; ou 0 se precisa manter a velocidade atual.
+//
+//        Restrições
+//        0 ≤ A < B < C ≤ 500
+
+        int a, b, c; // represetam posição do carro
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+
+        if ((b - a) < (c - b)) {
+            System.out.println(1);
+        } else if ((b - a) > (c - b)) {
+            System.out.println(-1);
+        } else {
+            System.out.println(0);
+        }
     }
 
+    public static void cifraDaNlogônia() {
+        //link: https://olimpiada.ic.unicamp.br/pratique/p2/2021/f1/cifra/
+        String consoante = "bcdfghjklmnpqrstvz";
+        String vogal_mais_prox = "aaeeeiiiiooooouuuuu";
+        String prox_consoante = "cdfghjklmnpqrstvxzz";
+        //Fiz na mao pq ia complicar muito fazer um milhao de if pra isso
 
+        String palavra, cifra;
+        cifra = "";
+        palavra = scanner.next();
+        for (String letra in palavra){
+
+        }
+
+    }
 }
